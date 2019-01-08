cf8d6f9621cec71f615d4557906bf673fe7d22f9
Hoàng Văn Huấn <38126411+huanhoang2121@users.noreply.github.com>
2019-01-08 09:09:16 +0700

    update

---
+++ BienKieuDuLieuToanTu/Sử dụng toán tử Assignment/hinhchunhat.js
@@ -0,0 +1,5 @@
+var width = parseInt(prompt("Nhập vào chiều rộng (cm):","0"));
+var height = parseInt(prompt("Nhập vào chiều dài (cm)","0"));
+var area = width * height;
+
+document.write("Diện tích của hình chữ nhật là: " + area +" cm2");
