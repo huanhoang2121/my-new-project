650d31e1318ff874a27ee445870122d71f0cf884
Hoàng Văn Huấn <38126411+huanhoang2121@users.noreply.github.com>
2019-01-23 14:43:24 +0700

    add

--- Lập trình hướng đối tượng (2)/Pikachu/script.js
+++ Lập trình hướng đối tượng (2)/Pikachu/script.js
@@ -5,7 +5,7 @@ function Hero(image, top, left, width, height){
     this.left = left;
     this.width = width;
     this.height = height;
-    this.speed = 20;
+    this.speed = 100;
 
     this.getHeroElement = function(){
         return '<img id="car" width="'+ this.width + '"' +
@@ -22,32 +22,31 @@ function Hero(image, top, left, width, height){
         this.top += this.speed;
         console.log('ok: ' + this.top);
     }
-    // this.moveLeft = function(){
-    //     this.left -= this.speed;
-    //     console.log('ok: ' + this.left);
-    // }
-    // this.moveTop = function(){
-    //     this.top -= this.speed;
-    //     console.log('ok: ' + this.top);
-    // }
-
+    this.moveLeft = function(){
+        this.left -= this.speed;
+        console.log('ok: ' + this.left);
+    }
+    this.moveTop = function(){
+        this.top -= this.speed;
+        console.log('ok: ' + this.top);
+    }
 }
 
 var hero = new Hero('car1.png', 20, 30, 100, 200);
 
 function start(){
-    if(hero.left < window.innerWidth - hero.width - 20){
-        hero.moveRight();
-    }
-    else if (hero.top < window.innerHeight - hero.height - 20) {
-        hero.moveBottom();
-    }
-    // else if (hero.left > window.innerWidth - hero.size - 20) {
-    //     hero.moveLeft();
-    // }
-    // else if (hero.top > window.innerHeight - hero.size - 20) {
-    //     hero.moveTop();
-    // }
+        if(hero.left < window.innerWidth - hero.width * 2){
+            hero.moveRight();
+        }
+        else if (hero.top < window.innerHeight - hero.height * 2) {
+            hero.moveBottom();
+        }
+        else if (hero.left < window.innerWidth + hero.width * 2) {
+            hero.moveLeft();
+        }
+        else if (hero.top < window.innerHeight + hero.height * 2) {
+            hero.moveTop();
+        }
     document.getElementById('game').innerHTML = hero.getHeroElement();
     setTimeout(start, 500)
 }
